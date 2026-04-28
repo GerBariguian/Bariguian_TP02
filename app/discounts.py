999903d9a35e545e5b4968b328885d812b0877e7
GerBariguian <gbariguian@uade.edu.ar>
2026-04-28 10:58:26 -0300

    Update discounts.py

--- app/discounts.py
+++ app/discounts.py
@@ -1,1 +1,11 @@
+def apply_discount(price, discount_percentage):
+    if price <= 0:
+        raise ValueError("El precio debe ser mayor que 0")
 
+    if discount_percentage < 0 or discount_percentage > 100:
+        raise ValueError("El descuento debe estar entre 0 y 100")
+
+    discount_amount = price * (discount_percentage / 100)
+    final_price = price - discount_amount
+
+    return final_price
